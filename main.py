cbc6bf5c2a1e982406019f4d12fd858987867228
Isaac-To <isaacto3890@gmail.com>
2023-11-10 21:20:37 +0000

    add bootstrap styling and dark theme

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from flask import *
 PROJECT_NAME = "Slug Counselor"
+BASEURL = "http://localhost:5000/"
 
 app = Flask(__name__, static_url_path="", static_folder="static", template_folder="templates")
 
 @app.route("/form")
 def form():
-    return render_template("form.html", PROJECT_NAME=PROJECT_NAME)
+    return render_template("form.html", PROJECT_NAME=PROJECT_NAME, BASEURL=BASEURL)
 
 @app.route("/")
 def root():
-    return render_template("index.html", PROJECT_NAME=PROJECT_NAME)
+    return render_template("index.html", PROJECT_NAME=PROJECT_NAME, BASEURL=BASEURL)
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
