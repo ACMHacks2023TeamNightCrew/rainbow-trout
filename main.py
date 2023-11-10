05dc3d1e158f3538d073a138b947aad1f1364384
Dmitry Pleshkov <dpleshkov.usa@gmail.com>
2023-11-10 12:54:47 -0800

    rudimentary api

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from flask import *
+import catalog_api
 
 app = Flask(__name__, static_url_path="", static_folder="static", template_folder="templates")
-
 
 
 @app.route("/")
 def root():
     return render_template("index.html")
 
+
+@app.route("/api/course/<course>")
+def get_course_info(course):
+    return catalog_api.get_course_info(course)
+
+
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run()
