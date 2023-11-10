5355e5fce17270b65d6f84a343cb34fc02c3611e
Isaac-To <isaacto3890@gmail.com>
2023-11-10 22:55:22 +0000

    about us page

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from flask import *
+TEAM_NAME = "Team Night Crew"
 PROJECT_NAME = "Slug Counselor"
-BASEURL = "http://localhost:5000/"
+BASEURL = "/"
 
 app = Flask(__name__, static_url_path="", static_folder="static", template_folder="templates")
 
+@app.route("/about-us")
+def aboutUs():
+    return render_template("about-us.html", TEAM_NAME=TEAM_NAME, PROJECT_NAME=PROJECT_NAME, BASEURL=BASEURL)
+
 @app.route("/form")
 def form():
-    return render_template("form.html", PROJECT_NAME=PROJECT_NAME, BASEURL=BASEURL)
+    return render_template("form.html", TEAM_NAME=TEAM_NAME, PROJECT_NAME=PROJECT_NAME, BASEURL=BASEURL)
 
 @app.route("/")
 def root():
-    return render_template("index.html", PROJECT_NAME=PROJECT_NAME, BASEURL=BASEURL)
+    return render_template("index.html", TEAM_NAME=TEAM_NAME, PROJECT_NAME=PROJECT_NAME, BASEURL=BASEURL)
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
