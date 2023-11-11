eee4ba36e16037cb2ede3312bfb718414da7500b
stewedbeef <114018527+stewedbeef@users.noreply.github.com>
2023-11-10 18:55:33 -0800

    prereqs fixed

--- prereqs.py
+++ prereqs.py
@@ -3,9 +3,9 @@
 def get(course: str):
     with open("prereqs.csv", "r") as f:
         for line in f:
-            s = line.split(", ")
+            s = line.strip().split(", ")
             if course == s[0]:
-                print(course, s[1:])
                 for i in range(len(s)):
                     s[i].strip("\n")
                 return s[1:]
+    return []
